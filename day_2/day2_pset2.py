0fe3c8754e59550a5651b59a358a043687095a8d
Rogerio Soares <rogeryu_gap@hotmail.com>
2022-12-08 15:22:24 +0000

    Day 2

---
+++ day_2/day2_pset2.py
@@ -0,0 +1,23 @@
+with open('day2_pset_input.txt', 'r') as strategy_guide:
+    strategy_guide = strategy_guide.readlines()
+    points = 0
+    for line in strategy_guide:
+        if "A X\n" == line:
+            points += (3 + 0)
+        if "A Y\n" == line:
+            points += (1 + 3)
+        if "A Z\n" == line:
+            points += (2 + 6)
+        if "B X\n" == line:
+            points += (1 + 0)
+        if "B Y\n" == line:
+            points += (2 + 3)
+        if "B Z\n" == line:
+            points += (3 + 6)
+        if "C X\n" == line:
+            points += (2 + 0)
+        if "C Y\n" == line:
+            points += (3 + 3)
+        if "C Z\n" == line:
+            points += (1 + 6)
+    print(points)
