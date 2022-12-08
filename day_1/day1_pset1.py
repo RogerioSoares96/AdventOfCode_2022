c5ecf23aeffd5a9575caa73006944e87d9ff7a74
Rogerio Soares <rogeryu_gap@hotmail.com>
2022-12-08 12:43:11 +0000

    Day 1 pset1

---
+++ day_1/day1_pset1.py
@@ -0,0 +1,12 @@
+calories_data = open("input.txt", "r").readlines()
+elfs_list = []
+elf_calories = 0
+for line in calories_data:
+    if line.strip():
+        elf_calories += int(line)
+    else:
+        elfs_list.append(elf_calories)
+        elf_calories = 0
+elfs_list.sort()
+print(elfs_list[len(elfs_list) - 1])
+
